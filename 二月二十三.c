b7f90b5b88b5dbffba40121d9db4b4e80fe79362
赵一帆 <2397184148@qq.com>
2021-02-23 18:54:30 +0800

    提交代码

---
+++ 二月二十三.c
@@ -0,0 +1,35 @@
+//int get_max(int x, int y)
+//{
+//    if (x > y)
+//        return x;
+//    else
+//        return y;
+//}
+//int main()
+//{
+//    int maxd = 0;
+//    int a = 10;
+//    int b = 20;
+//    int max=get_max(a, b);
+//   printf("max=%d\n", max);   
+//    maxd = get_max(100, 300);
+//    printf("max=%d\n", maxd);
+//    return 0;
+//}
+//int main()
+//{
+//    char arr[] = "hello world";
+//    memset(arr, '*', 5);
+//    printf("%s\n", arr);//memset-内存设置
+//    return 0;
+//}
+//int main()
+//{
+//    char arr1[] = "bit";
+//    char arr2[] = "##########";
+//    strcpy(arr2, arr1);
+//    printf("%s\n", arr2);
+//   //strlen-string lenth-求字符串长度
+//   // strcpy-复制字符串
+//    return 0;
+//}
