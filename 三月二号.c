a835fd6d8f616d19513f5c3b5da8b0ac0dada0ba
赵一帆 <2397184148@qq.com>
2021-03-02 14:25:26 +0800

    提交代码

---
+++ 三月二号.c
@@ -0,0 +1,43 @@
+//void print(int a)//递归
+//{
+//    if (a > 9)
+//    {
+//        print(a / 10);
+//    }
+//    printf("%d ", a % 10);
+//}
+//int main()
+//{
+//    unsigned int num = 0;
+//    scanf("%d", &num);
+//    print(num);
+//    return 0;
+//}
+//int main()
+//{
+//    printf("hehe\n");
+//    main();/// stack overflow--栈溢出
+//    return 0;
+//}
+//函数声明
+//int AdD(int x, int y);
+//int main()
+//{
+//    int a = 10;
+//    int b = 20;
+//    int sum = 0;
+//    sum = AdD(a, b);
+//    printf("%d ", sum);
+//    return 0;
+//}
+//函数的定义
+//int AdD(int a, int b)
+//{
+//    int z = a + b;
+//    return z;
+//}
+//int main()
+//{
+//    printf("%d", printf("%d", printf("%d", 43)));
+//    return 0;
+//}
